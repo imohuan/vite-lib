be6f0bc6710e0925f2d9533d909a7ca956f0e98d
梦念幻想 <5402106+imohuan@user.noreply.gitee.com>
2022-06-19 21:43:48 +0800

    x

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, it } from "vitest";
-import { log } from "../dist/imohuan-cli-cjs";
+import { log } from "../dist/lib-cjs";
 
 describe("测试", () => {
   it("测试 1", async () => {
